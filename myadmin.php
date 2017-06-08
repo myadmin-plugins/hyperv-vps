982f7742c4d5c98e3d9cf33f0bc74fd621c9de0f
Joe Huss <detain@interserver.net>
2017-06-08 00:59:19 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Hyperv Vps',
 	'description' => 'Allows selling of Hyperv Server and VPS License Types.  More info at https://www.netenberg.com/hyperv.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a hyperv license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'vps',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-hyperv-vps',
 	'repo' => 'https://github.com/detain/myadmin-hyperv-vps',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminHyperv\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminHyperv\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminHyperv\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminHyperv\Plugin', 'ChangeIp'],
+		'vps.settings' => ['Detain\MyAdminHyperv\Plugin', 'Settings'],
+		'vps.activate' => ['Detain\MyAdminHyperv\Plugin', 'Activate'],
+		'vps.change_ip' => ['Detain\MyAdminHyperv\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminHyperv\Plugin', 'Menu'] */
 	],
 ];
