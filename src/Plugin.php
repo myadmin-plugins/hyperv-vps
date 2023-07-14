8d93f2f15786e185e8c3ce25858057e82fea626f
Joe Huss <detain@interserver.net>
2023-07-14 19:13:40 -0400

    added code to the hyperv create to make sure the templates a valid one,and if not choose a valid one for the install.. this should fix several issues that occationally come up like people somehow getting a kvm templaet listed .. or a blank one

--- src/Plugin.php
+++ src/Plugin.php
@@ -272,6 +272,12 @@ class Plugin
         $params = \Detain\MyAdminHyperv\Plugin::getSoapClientParams();
         $url = "https://{$serviceInfo['server_info']['vps_ip']}/HyperVService/HyperVService.asmx?WSDL";
         $db->query("update vps set vps_server_status='{$progress}' where vps_id={$serviceInfo['id']}", __LINE__, __FILE__);
+        $db->query("select * from vps_templates where template_type=11 and template_available=1 and template_file='{$serviceInfo['vps_os']}'");
+        if ($db->num_rows() == 0) {
+            $db->query("select * from vps_templates where template_type=11 and template_available=1 limit 1");
+            $db->next_record(MYSQL_ASSOC);
+            $serviceInfo['vps_os'] = $db->Record['template_file'];
+        }
         if (is_uuid($serviceInfo['vps_vzid'])) {
             myadmin_log('hyperv', 'info', 'Existing UUID value found, attempting to delete', __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
             $parameters = [
